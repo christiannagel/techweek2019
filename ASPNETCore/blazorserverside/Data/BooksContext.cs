869bf2adb3ef84ea8a2f3ab6a3b1451ffefbcf14
Christian Nagel <christian@christiannagel.com>
2019-05-24 07:37:43 +0200

    add ef core to blazor

---
+++ ASPNETCore/blazorserverside/Data/BooksContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace blazorserverside.Data
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext(DbContextOptions<BooksContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
--- ASPNETCore/blazorserverside/Startup.cs
+++ ASPNETCore/blazorserverside/Startup.cs
@@ -9,17 +9,27 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using blazorserverside.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace blazorserverside
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddDbContext<BooksContext>(options => options.UseSqlServer(Configuration.GetConnectionString("BooksConnection")));
             services.AddSingleton<WeatherForecastService>();
         }
 
